da1951b466873307b6b66b0afee273d0f58617d5
Omagor <omagor@macbook-air-von-omagor.home>
2025-04-04 22:42:49 +0200

    correct names

--- backend/src/service/dto/goal_dto.py
+++ backend/src/service/dto/goal_dto.py
@@ -1,15 +1,18 @@
 import datetime
 class goal_dto:
-    def __init__(self, content, due_date):
-        self.content: str = content
-        self.due_date: datetime = due_date
+    def __init__(self, goal_id, description,creation_date, due_date, goal_type):
+        self.goal_id: str = goal_id
+        self.description: str = description
+        self.creation_date: datetime.date = creation_date
+        self.due_date: datetime.datetime = due_date
+        self.goal_tpye: str = goal_type
         
 
 class individual_goal_dto(goal_dto):
-    def __init__(self, rating):
-        self.rating: int = rating
+    def __init__(self, grading):
+        self.grading: int = grading
 
 class standard_goal_dto(goal_dto):
-    def __init__(self, rating):
-        self.rating: str = rating
+    def __init__(self, grading):
+        self.grading: str = grading
 
