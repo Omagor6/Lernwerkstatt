7a53a886d518f6026ed8e7b96535c6e15b31af6f
grubm4 <maria.gruber@students.bfh.ch>
2025-04-05 13:48:40 +0200

    fix get goals

--- backend/app/service/goal_service.py
+++ backend/app/service/goal_service.py
@@ -4,22 +4,20 @@ import datetime
 import json
 
 
-def get_goals(person_id):
+def get_goals(person_id): 
     goals = goal_repository.get_goals(person_id)
+    print(goals)
     for goal in goals:
-        goal["creation_date"] = goal.pop("created_timestamp").strftime("%d/%m/%Y") ##rename and assign
-        goal["creation_date"] = goal["due_date"].strftime("%d/%m/%Y, %H:%M:%S") ##rename and assign
+        goal["creation_date"] = goal.pop("created_timestamp").strftime("%d/%m/%Y")
+        goal["due_date"] = goal["due_date"].strftime("%d/%m/%Y")
         goal["student_id"] = str(goal["student_id"])
     return goals
+
 
 def post_goal(goal):
     description = goal.get("description")
     due_date = goal.get("due_date")
     goal_type = goal.get("goal_type")
     student_id = goal.get("student_id")
-    print(description)
-    print(due_date)
-    print(goal_type)
-    print(student_id)
     return goal_repository.post_goal(description, due_date, goal_type, student_id)
     
