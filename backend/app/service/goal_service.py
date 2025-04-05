05b841ad00719c5aaba67db47d242f2b741648f8
Omagor <omagor@MacBook-Air-von-Omagor.local>
2025-04-05 13:34:04 +0200

    str-conversion

--- backend/app/service/goal_service.py
+++ backend/app/service/goal_service.py
@@ -5,7 +5,12 @@ import json
 
 
 def get_goals(person_id):
-    return goal_repository.get_goals(person_id)
+    goals = goal_repository.get_goals(person_id)
+    for goal in goals:
+        goal["creation_date"] = goal.pop("created_timestamp").strftime("%d/%m/%Y") ##rename and assign
+        goal["creation_date"] = goal["due_date"].strftime("%d/%m/%Y, %H:%M:%S") ##rename and assign
+        goal["student_id"] = str(goal["student_id"])
+    return goals
 
 def post_goal(goal):
     description = goal.get("description")
